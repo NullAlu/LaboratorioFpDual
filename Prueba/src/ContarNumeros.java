804917618d147c6b3bc6712462c07e02769e5c4f
Testa Moreno <marco.testa.moreno@accenture.com>
2021-03-25 14:59:09 +0100

    Importando proyecto de FP dual, un ejenmplo

---
+++ Prueba/src/ContarNumeros.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContarNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero2=0;
+		System.out.println("Introduzca una cadena: ");
+		Scanner sc=new Scanner(System.in);
+		String cadena=sc.nextLine();
+		for(int i=0;i<cadena.length();i++) {
+			int numero = 0;
+			if(numero=='1' || numero=='2' || numero=='3'|| numero=='4'|| numero=='5'
+					|| numero=='6'|| numero=='7'|| numero=='8'|| numero=='9'
+					|| numero=='0') {
+				
+				
+
+			}
+		}System.out.println("Números en la cadena: " +numero2);
+		
+	}
+
+}
---
+++ Prueba/src/Hola.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hola {
+	
+		public static void main(String[] args){
+			String parametro="";
+			System.out.println("Introduce un parámetro: ");	
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Hola "+parametro);
+		
+		
+		
+		}
+		
+}
